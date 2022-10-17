2765ff689092495c36e5bdc17206440165aa0bfb
giorgi <gjanashia@unisens.ge>
2022-10-17 16:54:19 +0400

    exercise completed

--- printf/03-false-claims/main.go
+++ printf/03-false-claims/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: False Claims
+//
+//  Use printf to print the expected output using a variable.
+//
+// EXPECTED OUTPUT
+//  These are false claims.
+// ---------------------------------------------------------
+
 func main() {
-	
+	tf := false
+
+	fmt.Printf("These are %t claims\n", tf)
 }
