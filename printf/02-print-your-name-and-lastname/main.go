c21b904b81568bd2beacbc7e02c055d657613135
giorgi <gjanashia@unisens.ge>
2022-10-17 16:50:11 +0400

    exercise completed

--- printf/02-print-your-name-and-lastname/main.go
+++ printf/02-print-your-name-and-lastname/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Print Your Name and LastName
+//
+//  Print your name and lastname using Printf
+//
+// EXPECTED OUTPUT
+//  My name is Inanc and my lastname is Gumus.
+//
+// BONUS
+//  Store the formatting specifier (first argument of Printf)
+//    in a variable.
+//  Then pass it to printf
+// ---------------------------------------------------------
+
 func main() {
-	
+	// BONUS: Use a variable for the format specifier
+
+	m := "My name is %s and my lastname is %s.\n"
+
+	fmt.Printf(m, "Giorgi", "Janashia")
 }
